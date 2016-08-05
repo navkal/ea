40fc32a4c91b6d987f249e5dfb2f504b3719f96a
Naomi Navkal <navkal@hotmail.com>
2016-08-05 17:19:09 -0400

    Labels

---
+++ mda/labels.php
@@ -0,0 +1,14 @@
+<?php
+  $labels =
+  [
+    "metasysFile" => "Metasys File",
+    "analysisOptions" => "Analysis Options",
+    "summarize" => "Summarize",
+    "startTime" => "Start Time",
+    "period" => "Time Period",
+    "fullday" => "Full Day",
+    "partday" => "Partial Day",
+    "endTime" => "End Time",
+    "cost" => "Cost per kWh"
+  ];
+?>
