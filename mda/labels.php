82b3c70536ff942d99e8455627f9d2a77c540e55
Naomi Navkal <navkal@hotmail.com>
2016-08-26 17:08:52 -0400

    Fix syntax errors

--- mda/labels.php
+++ mda/labels.php
@@ -1,17 +1,17 @@
 <?php
-  define( METASYS_DATA_ANALYSIS, "Metasys Data Analysis" );
-  define( METASYS_FILE, "Metasys File" );
-  define( ANALYSIS_OPTIONS, "Analysis Options" );
-  define( REPORT_FORMAT, "Report Format" );
-  define( SUMMARY, "Summary" );
-  define( DETAILED, "Detailed" );
-  define( MULTIPLE, "Multiple" );
-  define( START_TIME, "Start Time" );
-  define( TIME_PERIOD, "Time Period" );
-  define( FULL_DAY, "Full Day" );
-  define( PARTIAL_DAY, "Partial Day" );
-  define( END_TIME, "End Time" );
-  define( COST_PER_KWH, "Cost per kWh" );
-  define( POINTS_OF_INTEREST, "Points of Interest" );
-  define( RESULTS_FILE, "Results File" );
+  define( "METASYS_DATA_ANALYSIS", "Metasys Data Analysis" );
+  define( "METASYS_FILE", "Metasys File" );
+  define( "ANALYSIS_OPTIONS", "Analysis Options" );
+  define( "REPORT_FORMAT", "Report Format" );
+  define( "SUMMARY", "Summary" );
+  define( "DETAILED", "Detailed" );
+  define( "MULTIPLE", "Multiple" );
+  define( "START_TIME", "Start Time" );
+  define( "TIME_PERIOD", "Time Period" );
+  define( "FULL_DAY", "Full Day" );
+  define( "PARTIAL_DAY", "Partial Day" );
+  define( "END_TIME", "End Time" );
+  define( "COST_PER_KWH", "Cost per kWh" );
+  define( "POINTS_OF_INTEREST", "Points of Interest" );
+  define( "RESULTS_FILE", "Results File" );
 ?>
