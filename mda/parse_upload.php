2d4b548a557ac73d1ff73973c815364b84ea7e5a
Naomi Navkal <navkal@hotmail.com>
2016-08-31 14:19:44 -0400

    Determine which Points of Interest are summarizable

--- mda/parse_upload.php
+++ mda/parse_upload.php
@@ -101,30 +101,33 @@
     error_log( "===========> AF COLUMNS" );
 
     // Replace properties with format used by client
+    define( "THRESHOLD", 0.0005 );  // 0.00037950664136623 is sufficiently small according to available sample input files
+
     foreach( $colMap as $key => $properties )
     {
-      $volatility = 1;
-      $total =  $properties["lt"] + $properties["gt"] + $properties["eq"];
+      $totalDeltas =  $properties["lt"] + $properties["gt"] + $properties["eq"];
+
+      $volatility = THRESHOLD + 1;
       if ( $properties["first"] < $properties["value"] )
       {
-        $volatility = $properties["lt"] / $total;
+        $volatility = $properties["lt"] / $totalDeltas;
       }
       else if ( $properties["first"] > $properties["value"] )
       {
-        $volatility = $properties["gt"] / $total;
+        $volatility = $properties["gt"] / $totalDeltas;
       }
 
-      $summarizable = $volatility < 0.0005;  // 0.00037950664136623 is sufficient based on testing
-      error_log( "=========> key=" . $key . " volatility=" . $volatility . " regressions=" . ( $volatility * $total ) . " summarizable=" . $summarizable );
+      $summarizable = $volatility < THRESHOLD;
+      error_log( "=========> key=" . $key . " volatility=" . $volatility . " regressions=" . ( $volatility * $totalDeltas ) . " summarizable=" . $summarizable );
 
       $colMap[$key] = ["summarizable" => $summarizable ];
-      $old =
+      $oldMethod =
         ( ( ( $properties["lt"] ) <= 2 ) && ( $properties["first"] < $properties["value"] ) )
         ||
         ( ( ( $properties["gt"] ) <= 2 ) && ( $properties["first"] > $properties["value"] ) )
         ;
 
-      if ( $old != $summarizable ) error_log( "============DIFFERENT!================" );
+      if ( $oldMethod != $summarizable ) error_log( "============DIFFERENT!================" );
     }
 
     ksort( $colMap );
