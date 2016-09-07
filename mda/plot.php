e837b0655d2fbac68577a86b6f35e674adb89662
Naomi Navkal <navkal@hotmail.com>
2016-09-07 10:56:31 -0400

    Purge unused plot features

--- mda/plot.php
+++ mda/plot.php
@@ -65,12 +65,8 @@ function loadPlot()
 }
 </script>
 
-<div class="row">
-  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    <div class="panel-body">
-      <?php
-        include( "flotPlot.php" );
-      ?>
-    </div>
-  </div>
-</div>
+
+<?php
+  include( "flotPlot.php" );
+?>
+
