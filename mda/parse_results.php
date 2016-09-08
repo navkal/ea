1295bf82323a5c9e212242f33a02503d5d613f53
Naomi Navkal <navkal@hotmail.com>
2016-09-08 11:39:28 -0400

    Purge unused plot features

--- mda/parse_results.php
+++ mda/parse_results.php
@@ -17,27 +17,28 @@
         <p class="h3"><?=METASYS_DATA_ANALYSIS?> results</p>
       </div>
     </div>
-    <div class="container-fluid">
+
+    <?php
+      include( "plot.php" );
+    ?>
+    <!-------------------------------------------- >
+    <div class="form-group">
+      <label class="control-label" for="cost"><?=COST_PER_KWH?></label>
+      <div class="input-group">
+        <span class="input-group-addon" id="dollars">$</span>
+        <input type="number" value="0.16" min="0.01" step="0.01" class="form-control" id="cost" name="cost" />
+      </div>
+    </div>
+    <!-------------------------------------------->
+
+    <div class="container">
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <?php
-            include( "plot.php" );
-          ?>
-          <!-------------------------------------------- >
-          <div class="form-group">
-            <label class="control-label" for="cost"><?=COST_PER_KWH?></label>
-            <div class="input-group">
-              <span class="input-group-addon" id="dollars">$</span>
-              <input type="number" value="0.16" min="0.01" step="0.01" class="form-control" id="cost" name="cost" />
-            </div>
+          <!-- Close button -->
+          <div style="text-align:center;" >
+           <a class="btn btn-default" href="javascript:startClose()" role="button">Close</a>
           </div>
-          <!-------------------------------------------->
         </div>
-      </div>
-
-      <!-- Close button -->
-      <div style="text-align:center;" >
-       <a class="btn btn-default" href="javascript:startClose()" role="button">Close</a>
       </div>
     </div>
 
