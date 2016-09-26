b83b23a02bd58afb2a07b83fa7b97be190767b5c
Naomi Navkal <navkal@hotmail.com>
2016-09-26 19:48:57 -0400

    Reorganize layout of Plot view

--- mda/plot.php
+++ mda/plot.php
@@ -220,8 +220,8 @@
       <div class="panel-body">
         <div class="row">
           <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-              <button type="button" id="downSampleToDefault" class="btn btn-default btn-sm" style="margin:6px" onclick="plotDownSampleToDefault();return false;" title="Show default plot view" >Zoom Out to Full Range</button>
-              <button type="button" id="downSampleToRange" class="btn btn-default btn-sm" style="margin:6px" onclick="plotDownSampleToRange();return false;" title="Zoom overview plot to selected range" >Zoom In to Selected Range</button>
+              <button type="button" id="downSampleToDefault" class="btn btn-default btn-sm" style="margin:6px" onclick="plotDownSampleToFullRange();return false;" title="Show default plot view" >Zoom Out to Full Range</button>
+              <button type="button" id="downSampleToRange" class="btn btn-default btn-sm" style="margin:6px" onclick="plotDownSampleToSelectedRange();return false;" title="Zoom overview plot to selected range" >Zoom In to Selected Range</button>
           </div>
           <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
             <div class="checkbox" >
