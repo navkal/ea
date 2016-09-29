20230e7ed4298b936e0465eb72115574dbd3a63e
Naomi Navkal <navkal@hotmail.com>
2016-09-29 11:13:38 -0400

    Plot Help

--- mda/parse_help.php
+++ mda/parse_help.php
@@ -9,8 +9,8 @@
       <div class="modal-body bg-info">
         <dl>
           <dd>
-            A <i><?=METASYS_FILE?></i> is a .csv file exported from Metasys.    
-          </dd>            
+            A <i><?=METASYS_FILE?></i> is a .csv file exported from Metasys.
+          </dd>
         </dl>
         <dl>
           <dd>
@@ -154,6 +154,7 @@
         <h4 class="modal-title" id="helpColumnsLabel"><?=POINTS_OF_INTEREST?></h4>
       </div>
       <div class="modal-body bg-info">
+
         <dl>
           <dt>
             Available <?=POINTS_OF_INTEREST?>
@@ -203,13 +204,13 @@
             </dl>
           </dd>
         </dl>
+
+
         <dl>
           <dt>
             Selected <?=POINTS_OF_INTEREST?>
           </dt>
         </dl>
-
-
         <dl class="padLeftSmall">
           <dd>
             <dl class="dl-horizontal" >
@@ -254,18 +255,86 @@
 
 <!-- Modal dialog for Plot help -->
 <div class="modal fade" id="helpPlot" tabindex="-1" role="dialog" aria-labelledby="helpPlotLabel">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="helpPlotLabel"><?=METASYS_DATA_ANALYSIS_RESULTS?></h4>
       </div>
       <div class="modal-body bg-info">
+
         <dl>
+          <dt>
+            <?=POINTS_OF_INTEREST?>
+          </dt>
           <dd>
-            This is a stub.
+            Use checkboxes and and buttons to select for display in plot.
           </dd>
         </dl>
+
+        <dl>
+          <dt>
+            Plot
+          </dt>
+          <dd>
+            <ul>
+              <li>
+                Drag mouse across plot to select range.
+              </li>
+              <li>
+                Click plot to deselect range.
+              </li>
+              <li>
+                Use scrollbar to pan selected range.
+              </li>
+              <li>
+                Use buttons below plot to zoom in and out of selected range.
+              </li>
+              <li>
+                Use checkboxes below plot to customize display.
+              </li>
+            </ul>
+          </dd>
+        </dl>
+
+        <br/>
+        <h4>Advanced</h4>
+
+        <dl>
+          <dt>
+            Down Sample by Density
+          </dt>
+          <dd>
+            <ul>
+              <li>
+                <b>Density</b>: Maximum number of samples to show, evenly distributed throughout range
+              </li>
+              <li>
+                <b>Offset</b>: Index of first sample
+              </li>
+            </ul>
+          </dd>
+        </dl>
+
+        <dl>
+          <dt>
+            Down Sample by Pattern
+          </dt>
+          <dd>
+            <ul>
+              <li>
+                <b>Show</b>: Number of samples to show in alternating pattern
+              </li>
+              <li>
+                <b>Hide</b>: Number of samples to hide in alternating pattern
+              </li>
+              <li>
+                <b>Offset</b>: Index of first sample
+              </li>
+            </ul>
+          </dd>
+        </dl>
+
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
