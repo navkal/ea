32f8ded280ed36bdc54185b1e2e9f1397586c1fe
Naomi Navkal <navkal@hotmail.com>
2016-10-02 15:58:00 -0400

    Branding

--- ms/ms.php
+++ ms/ms.php
@@ -1,3 +1,3 @@
 <div class="container">
-  <a href="http://www.energizeAndover.com" target="_blank"><h3>Energize Andover</h3></a>
+  <a href="http://www.energizeAndover.com" target="_blank"><h3>Energize Andover Website</h3></a>
 </div>
