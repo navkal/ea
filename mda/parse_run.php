9d79aef6bd057f922db94329528216c0c97f80fb
Naomi Navkal <navkal@hotmail.com>
2016-10-19 16:10:42 -0400

    Archive uploaded input files

--- mda/parse_run.php
+++ mda/parse_run.php
@@ -239,7 +239,7 @@
 
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= "From: EnergizeApps@gmail.com <SmtpDispatch@gmail.com>" . "\r\n";
+    $headers .= "From: Energize Apps <SmtpDispatch@gmail.com>" . "\r\n";
 
     mail( $to, $subject, $text, $headers );
   }
