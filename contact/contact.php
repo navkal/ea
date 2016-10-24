d470b32071c6e40083a141603b899333938d92ac
Naomi Navkal <navkal@hotmail.com>
2016-10-24 16:31:56 -0400

    Common contact code

--- contact/contact.php
+++ contact/contact.php
@@ -2,18 +2,7 @@
 
 <div class="container">
   <?php
-
-    require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
     require_once $_SERVER["DOCUMENT_ROOT"]."/../common/contact.php";
-    error_log( "====> post=" . print_r( $_POST, true ) );
-
-    if ( count( $_POST ) == 0 )
-    {
-      showContactForm( "h3", "Energize Apps" );
-    }
-    else
-    {
-      sendContactMessage( "EnergizeApps@gmail.com", "We", "The Energize Apps team" );
-    }
+    contact( "h3", "Energize Apps", "EnergizeApps@gmail.com", "We", "The Energize Apps team" );
   ?>
 </div>
