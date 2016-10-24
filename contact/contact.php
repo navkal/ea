8d09d1784e89fd7307757358c6437346f9125ba7
Naomi Navkal <navkal@hotmail.com>
2016-10-24 16:20:23 -0400

    Common contact code

--- contact/contact.php
+++ contact/contact.php
@@ -1,48 +1,19 @@
-<style>
-  body
-  {
-    background-image: url( "contact/bg.jpg" );
-    background-position: center top;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-size: cover;
-  }
-
-  label, p.h3, p.h2, .form-control
-  {
-    color: white;
-  }
-
-  .form-control
-  {
-    background-color: transparent;
-  }
-</style>
+<!-- Copyright 2016 Energize Apps.  All rights reserved. -->
 
 <div class="container">
+  <?php
 
-<?php
-  require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
-  error_log( "====> post=" . print_r( $_POST, true ) );
+    require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
+    require_once $_SERVER["DOCUMENT_ROOT"]."/../common/contact.php";
+    error_log( "====> post=" . print_r( $_POST, true ) );
 
-  if ( count( $_POST ) == 0 )
-  {
-    showContactForm( "h3", "Energize Apps" );
-  }
-  else
-  {
-    sendContactMessage( "EnergizeApps@gmail.com", "We", "The Energize Apps team" );
-  }
-?>
-
+    if ( count( $_POST ) == 0 )
+    {
+      showContactForm( "h3", "Energize Apps" );
+    }
+    else
+    {
+      sendContactMessage( "EnergizeApps@gmail.com", "We", "The Energize Apps team" );
+    }
+  ?>
 </div>
-
-<script>
-  function onSubmitContact()
-  {
-    $( ".form-control" ).prop( "readonly", true );
-    $( "#submitButton" ).prop( "disabled", true );
-    $( "#cancelButton" ).prop( "disabled", true );
-    return true;
-  }
-</script>
