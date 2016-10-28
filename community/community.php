44f2d11c2033af03f25f81364a60f6f871dc3f5c
Naomi Navkal <navkal@hotmail.com>
2016-10-28 15:53:15 -0400

    Community page

--- community/community.php
+++ community/community.php
@@ -1,3 +1,32 @@
+<?php
+  require_once $_SERVER["DOCUMENT_ROOT"]."/mda/labels.php";
+?>
+
 <div class="container">
-  <a href="http://www.energizeAndover.com" target="_blank"><h3>Energize Andover Website</h3></a>
+
+  <div class="page-header">
+    <p class="h3">Community</p>
+  </div>
+
+  <div class="list-group">
+    <a href="http://www.energizeAndover.com" target="_blank" class="list-group-item">
+      <h4 class="list-group-item-heading">
+        <span class="text-info">Energize Andover<span>
+      </h4>
+      <p class="list-group-item-text">A collaboration between Andover, MA Plant and Facilities staff and Andover area students</p>
+    </a>
+    <a href="http://www.VirajNavkal.com" target="_blank" class="list-group-item">
+      <h4 class="list-group-item-heading">
+        <span class="text-info">Viraj Navkal<span>
+      </h4>
+      <p class="list-group-item-text"><?=METASYS_DATA_ANALYSIS?> algorithm developer and data scientist</p>
+    </a>
+    <a href="http://bitbybitworks.wordpress.com/" target="_blank" class="list-group-item">
+      <h4 class="list-group-item-heading">
+        <span class="text-info">Bit by Bit<span>
+      </h4>
+      <p class="list-group-item-text">Blog on sustainable urban design and data technology, by Legg Yeung</p>
+    </a>
+  </div>
+
 </div>
--- ms/ms.php
+++
@@ -1,3 +0,0 @@
-<div class="container">
-  <a href="http://www.energizeAndover.com" target="_blank"><h3>Energize Andover Website</h3></a>
-</div>
