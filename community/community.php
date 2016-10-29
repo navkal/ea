56ce36bc1816b691f2c9fa72af11487aee466f0e
Naomi Navkal <navkal@hotmail.com>
2016-10-29 14:12:48 -0400

    Page title

--- community/community.php
+++ community/community.php
@@ -2,31 +2,47 @@
   require_once $_SERVER["DOCUMENT_ROOT"]."/mda/labels.php";
 ?>
 
+<style>
+.list-group-item
+{
+  border-width:0px;
+}
+</style>
 <div class="container">
 
   <div class="page-header">
     <p class="h3">Community</p>
   </div>
 
-  <div class="list-group">
-    <a href="http://www.energizeAndover.com" target="_blank" class="list-group-item">
-      <h4 class="list-group-item-heading">
-        <span class="text-info">Energize Andover<span>
-      </h4>
-      <p class="list-group-item-text">A collaboration between municipal Plant and Facilities staff and local students, in Andover, MA</p>
-    </a>
-    <a href="http://www.VirajNavkal.com" target="_blank" class="list-group-item">
-      <h4 class="list-group-item-heading">
-        <span class="text-info">Viraj Navkal<span>
-      </h4>
-      <p class="list-group-item-text"><?=METASYS_DATA_ANALYSIS?> algorithm developer and data scientist</p>
-    </a>
-    <a href="http://bitbybitworks.wordpress.com/" target="_blank" class="list-group-item">
-      <h4 class="list-group-item-heading">
-        <span class="text-info">Bit by Bit<span>
-      </h4>
-      <p class="list-group-item-text">Blog on sustainable urban design and data technology, by architect Legg Yeung</p>
-    </a>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <a href="http://www.energizeAndover.com" target="_blank" class="list-group-item">
+        <h4 class="list-group-item-heading">
+          <span class="text-info">Energize Andover<span>
+        </h4>
+        <p class="list-group-item-text">A collaboration between municipal Plant and Facilities staff and local students, in Andover, MA</p>
+      </a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <a href="http://www.VirajNavkal.com" target="_blank" class="list-group-item">
+        <h4 class="list-group-item-heading">
+          <span class="text-info">Viraj Navkal<span>
+        </h4>
+        <p class="list-group-item-text"><?=METASYS_DATA_ANALYSIS?> algorithm developer and data scientist</p>
+      </a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <a href="http://bitbybitworks.wordpress.com/" target="_blank" class="list-group-item">
+        <h4 class="list-group-item-heading">
+          <span class="text-info">Bit by Bit<span>
+        </h4>
+        <p class="list-group-item-text">Blog on sustainable urban design and data technology, by architect Legg Yeung</p>
+      </a>
+    </div>
   </div>
 
 </div>
