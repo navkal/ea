6c905081cca6291dbae2997cad02e6ae1fa4c373
Naomi Navkal <navkal@hotmail.com>
2016-10-29 10:44:36 -0400

    About page

--- about/about.php
+++ about/about.php
@@ -6,10 +6,26 @@
 
   <div class="page-header">
     <p class="h3">Energize Apps</p>
-    <p>Suite of energy-related data analysis applications (only one so far)</p>
+    <p>Suite of energy-related data analysis applications</p>
   </div>
 
-  <h4><?=METASYS_DATA_ANALYSIS?></h4>
-  <p>Analyzes data exported from <a href="http://www.johnsoncontrols.com/buildings/building-management/building-automation-systems-bas" target="_blank" >Metasys Building Automation System</a>.</p>
+  <br/>
+  <h3>Applications</h3>
+  <dl class="dl-horizontal">
+    <dt><?=METASYS_DATA_ANALYSIS?></dt>
+    <dd>Analyzes data exported from <a href="http://www.johnsoncontrols.com/buildings/building-management/building-automation-systems-bas" target="_blank" >Metasys Building Automation System</a></dd>
+  </dl>
+
+  <br/>
+  <h3>Team</h3>
+  <dl class="dl-horizontal">
+    <dt>Anil Navkal</dt>
+    <dd>Director</dd>
+    <dt><a href="http://www.VirajNavkal.com" target="_blank">Viraj Navkal</a></dt>
+    <dd>Developer, data analysis algorithms</dd>
+
+    <dt>Naomi Navkal</dt>
+    <dd>Developer, user interface</dd>
+  </dl>
 
 </div>
