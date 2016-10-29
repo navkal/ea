a0126bdac937aa33ec97f39ff46a95d1a94b2da1
Naomi Navkal <navkal@hotmail.com>
2016-10-29 12:10:12 -0400

    About page

--- about/about.php
+++ about/about.php
@@ -10,14 +10,14 @@
   </div>
 
   <br/>
-  <h3>Applications</h3>
+  <h4>Applications</h4>
   <dl class="dl-horizontal">
     <dt><?=METASYS_DATA_ANALYSIS?></dt>
     <dd>Analyzes data exported from <a href="http://www.johnsoncontrols.com/buildings/building-management/building-automation-systems-bas" target="_blank" >Metasys Building Automation System</a></dd>
   </dl>
 
   <br/>
-  <h3>Team</h3>
+  <h4>Team</h4>
   <dl class="dl-horizontal">
     <dt>Anil Navkal</dt>
     <dd>Director</dd>
