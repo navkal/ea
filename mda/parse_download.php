1de1ca9ccc28465ae36b3d497341f99a8ad37e21
Naomi Navkal <navkal@hotmail.com>
2016-10-31 20:44:38 -0400

    Download button

--- mda/parse_download.php
+++ mda/parse_download.php
@@ -3,11 +3,7 @@
 
   require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
 
-  error_log( "=========> in parse_download" );
-
   $downloadFilename = $_SESSION["completion"]["downloadFilename"];
   $downloadType = $_SESSION["completion"]["downloadType"];
-  error_log( "=========> bf downloadFile" );
   downloadFile( $downloadFilename, $downloadType );
-  error_log( "=========> af downloadFile" );
 ?>
