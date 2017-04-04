feab780a253656c3b7d2a43a09efe227949fdb0b
Naomi Navkal <navkal@hotmail.com>
2017-04-04 13:35:10 -0400

    Touch screen experiment

--- mda/plot/plot.php
+++ mda/plot/plot.php
@@ -36,12 +36,11 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.selection.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.time.min.js"></script>
 
-<script language="javascript" type="text/javascript" src="../../mda/plot/analyzer.js?version=<?=$timestamp?>"></script>
-<script language="javascript" type="text/javascript" src="../../mda/plot/scrollbar.js"></script>
-
 <!-- Flot add-on to support touch events -->
 <script language="javascript" type="text/javascript" src="../../mda/plot/jquery.flot.touch.js"></script>
 
+<script language="javascript" type="text/javascript" src="../../mda/plot/analyzer.js?version=<?=$timestamp?>"></script>
+<script language="javascript" type="text/javascript" src="../../mda/plot/scrollbar.js"></script>
 <link rel="stylesheet" href="../../util/util.css?version=<?=$timestamp?>">
 
 <style>
