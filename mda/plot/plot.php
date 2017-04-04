1baef951a2d870c470cf9b4bac4fc3315bf0931a
Naomi Navkal <navkal@hotmail.com>
2017-04-04 13:49:17 -0400

    Touch screen experiment

--- mda/plot/plot.php
+++ mda/plot/plot.php
@@ -33,11 +33,12 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.crosshair.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.navigate.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.resize.min.js"></script>
+<script language="javascript" type="text/javascript" src="../../mda/plot/selection.js?version=<?=$timestamp?>"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.time.min.js"></script>
 
-<script language="javascript" type="text/javascript" src="../../mda/plot/selection.js?version=<?=$timestamp?>"></script>
 <script language="javascript" type="text/javascript" src="../../mda/plot/analyzer.js?version=<?=$timestamp?>"></script>
 <script language="javascript" type="text/javascript" src="../../mda/plot/scrollbar.js?version=<?=$timestamp?>"></script>
+
 <link rel="stylesheet" href="../../util/util.css?version=<?=$timestamp?>">
 
 <style>
